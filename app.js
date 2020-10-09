3d618d3a399c2a47b2199f18af55b649940bda2b
amandena <manda.m.johnson@gmail.com>
2020-10-08 23:05:19 -0400

    created event listener with cardsArray and grid variable

---
+++ app.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+  //card options
+  const cardArray = [
+    {
+      name: 'jupiter',
+      img: '/assets/jupiter.png'
+    },
+    {
+      name: 'jupiter',
+      img: '/assets/jupiter.png'
+    },
+    {
+      name: 'mars',
+      img: '/assets/mars.png'
+    },
+    {
+      name: 'mars',
+      img: '/assets/mars.png'
+    },
+    {
+      name: 'mercury',
+      img: '/assets/mercury.png'
+    },
+    {
+      name: 'mercury',
+      img: '/assets/mercury.png'
+    },
+    {
+      name: 'neptune',
+      img: '/assets/neptune.png'
+    },
+    {
+      name: 'neptune',
+      img: '/assets/neptune.png'
+    },
+    {
+      name: 'sailormoon',
+      img: '/assets/sailormoon.png'
+    },
+    {
+      name: 'sailormoon',
+      img: '/assets/sailormoon.png'
+    },
+    {
+      name: 'tuxedomask',
+      img: '/assets/tuxedomask.png'
+    },
+    {
+      name: 'tuxedomask',
+      img: '/assets/tuxedomask.png'
+    },
+    {
+      name: 'uranus',
+      img: '/assets/uranus.png'
+    },
+    {
+      name: 'uranus',
+      img: '/assets/uranus.png'
+    },
+    {
+      name: 'venus',
+      img: '/assets/venus.png'
+    },
+    {
+      name: 'venus',
+      img: '/assets/venus.png'
+    }
+  ]
+
+  const grid = document.querySelector('.grid')
+})
